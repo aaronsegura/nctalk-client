37bd6aa70f0e0e26d40d7de6cc7fe7e1dfeac868
Aaron Segura <aaron@aaronsegura.com>
2022-06-16 08:19:46 -0600

    Load configuration from file, where available

--- nctalk_client/app.py
+++ nctalk_client/app.py
@@ -1,10 +1,14 @@
 import asyncio
 import logging
+import os
+import platform
+import json
 
 import datetime as dt
 import tkinter as tk
 
 from tkinter import DISABLED, NORMAL, ttk
+from tkinter import messagebox
 from tkinter.scrolledtext import ScrolledText
 
 from nctalk_client.login import LoginWindow
@@ -31,17 +35,41 @@ class MyApp(tk.Tk):
     def run(self):
         self.loop.run_forever()
 
+    def load_config(self) -> dict:
+        """Load cloud information from config file, if one exists."""
+        match platform.system():
+            case 'Linux':
+                config_filename = f'{os.environ["HOME"]}/.nctalk-client'
+            case 'Darwin':
+                config_filename = f'{os.environ["HOME"]}/Library/Preferences/'
+            case _:
+                messagebox.showerror(
+                    title='Unknown Platform',
+                    message=f'Platform {platform.system()} unknown.')
+                self.close()
+
+        try:
+            with open(config_filename, "r") as config_file:
+                config = json.loads(config_file.read())
+        except IOError:
+            config = {}
+
+        return config
+
     async def mainloop(self, interval: float = 1/120):
         while True:
             self.update()
             await asyncio.sleep(interval)
 
     def log(self, text: str, level: int = logging.INFO):
+        """Write a message to the application log window and console."""
         now = dt.datetime.now().strftime(r'%Y/%m/%d %H:%M:%S')
         line = f'{now} - {text}'
-        self.applog['state'] = NORMAL
+
+        # TODO: Text colors based on log level
+        self.applog.config(state=NORMAL)
         self.applog.insert(tk.INSERT, f'{line}\n')
-        self.applog['state'] = DISABLED
+        self.applog.config(state=DISABLED)
         self.applog.update()
         print(line)
 
@@ -54,35 +82,29 @@ class MyApp(tk.Tk):
         frame = ttk.Frame(self)
         frame.pack(fill='both', expand=True)
 
+        config = self.load_config()
+
         if not self.logged_in:
-            # TODO: Remove these params
-            login_window = LoginWindow(
-                self,
-                user='aaron',
-                endpoint='https://cloud.aaronsegura.com'
-            )
+            login_window = LoginWindow(self, **config)
             login_window.window.protocol("WM_DELETE_WINDOW", self.close)
             self.loop.create_task(self.wait_for_auth())
 
         self.room_tabs = ttk.Notebook(frame)
         self.room_tabs.pack(fill='both', expand=True)
 
-        applog_frame = ttk.Frame(self.room_tabs)
-        applog_frame.pack(fill='both', expand=True)
-
-        self.applog = ScrolledText(applog_frame, height=50, width=120)
+        self.applog = ScrolledText(self.room_tabs, height=50, width=120)
         self.applog.pack(fill='both', expand=True, padx=1, pady=1)
 
-        self.room_tabs.add(applog_frame, text='Talk Log')
+        self.room_tabs.add(self.applog, text='nctalk-client')
 
         self.log("Startup")
 
     async def wait_for_auth(self):
         """Wait for LoginWindow() to populate the NextCloudTalk client."""
         while not self.nct:
-            await asyncio.sleep(1/20)
+            await asyncio.sleep(1/5)
         else:
-            await self.initialize_rooms()
+            self.initialize_rooms()
 
     def initialize_rooms(self):
         """Open tabs for each room and initialize Room objects."""
@@ -91,10 +113,14 @@ class MyApp(tk.Tk):
 
         for c in rooms:
             self.log(f'Joining room "{c.displayName}"')
-            self.new_room(c)
+            self.loop.create_task(self.new_room(c))
 
     async def new_room(self, conversation):
-        Room(conversation)
+        room = Room(self, conversation)
+        self.rooms.append(room)
+
+        room_text = ScrolledText(self.room_tabs)
+        self.room_tabs.add()
 
     def close(self):
         for task in self.tasks:
